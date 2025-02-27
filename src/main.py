5bfde6ea1c965f202394bb0aeeb2e4778000d51c
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-02-27 21:49:47 +0100

    Write functionality for browsing files

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter.filedialog import askopenfilename
+
+
+def browse():
+    file_path = askopenfilename(
+        initialdir='/',
+        title="Select File",
+        filetypes=(('Image Files', '*.jpg'), ('Image Files', '*.jpeg'), ('All Files', '*.*'))
+    )
+    file_explorer.configure(text="File Opened: "+file_path)
+
 
 # Window initialisation and setup
 root = tk.Tk()
@@ -11,8 +22,12 @@ frm = ttk.Frame(root, padding=10)
 frm.grid()
 # Elements Initialisation
 exit_btn = ttk.Button(frm, text="Quit", command=root.destroy)
+file_explorer = ttk.Label(frm, text="Explore files")
+browse_btn = ttk.Button(frm, text="Browse Folder", command=browse)
 # Setting up elements on grid
-exit_btn.grid(column=0, row=0)
+file_explorer.grid(column=0, row=0)
+exit_btn.grid(column=0, row=1)
+browse_btn.grid(column=1, row=1)
 
 # Running window
 root.mainloop()
