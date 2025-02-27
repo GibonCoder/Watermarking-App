a6dc41684fb3128636373a11e485d171051c0c16
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-02-27 19:24:30 +0100

    Create Interface File

--- src/interface.py
+++ src/interface.py
@@ -1,1 +1,10 @@
 import tkinter as tk
+
+
+class Interface:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Watermark Your Images!")
+        self.root.geometry("800x800")
+        self.root.resizable(False, False)
+        self.root.mainloop()
