772931ae1979ef7ffff6da1a5d44a0596ceb59ba
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-02-27 19:35:42 +0100

    Write Interface class. Write method for initializing window

--- src/interface.py
+++ src/interface.py
@@ -1,10 +1,20 @@
 import tkinter as tk
+from tkinter import ttk
 
 
 class Interface:
     def __init__(self):
-        self.root = tk.Tk()
-        self.root.title("Watermark Your Images!")
-        self.root.geometry("800x800")
-        self.root.resizable(False, False)
-        self.root.mainloop()
+        self._root = tk.Tk()
+
+    def setup_window(self):
+        # Window Setup
+        self._root.title("Watermark Your Images!")
+        self._root.geometry("800x800")
+        self._root.resizable(False, False)
+        # Frame Setup
+        frm = ttk.Frame(self._root, padding=10)
+        frm.grid()
+        # Elements Setup
+        ttk.Label(frm, text="Select Image:").grid(column=0, row=0)
+        ttk.Button(frm, text="Quit", command=self._root.destroy).grid(column=1, row=0)
+        self._root.mainloop()
