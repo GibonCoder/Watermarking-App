b79c7880cede80f66e5ae1d86fd0f4a7a594f375
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-02-28 19:36:16 +0100

    Refactor functions

--- src/file_actions.py
+++ src/file_actions.py
@@ -2,17 +2,18 @@ from tkinter.filedialog import askopenfilename
 from PIL import Image, ImageTk
 
 
-def browse_files(label):
+def browse_files(exp_lbl, canvas):
     file_path = askopenfilename(
         initialdir='/',
         title="Select File",
         filetypes=[('Image Files', '*.jpg *.jpeg *.png')]
     )
     if not len(file_path) == 0:
-        label.configure(text=f"File Opened: {file_path}")
+        exp_lbl.configure(text=f"File Opened: {file_path}")
+        display_image(file_path, canvas)
 
 
-def display_image(path, label):
+def display_image(path, canvas):
     img = Image.open(path)
     photo = ImageTk.PhotoImage(img)
-    label.configure(image=photo)
+    canvas.create_image(10, 10, image=photo)
