6475a35d35d30d2aa86e8782ab30b2afdd359f22
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-03-03 21:44:51 +0100

    Create Radio Buttons for selecting theway how to watermark the photo

--- src/main.py
+++ src/main.py
@@ -18,14 +18,24 @@ root.geometry("800x800")
 
 img_container = []  # List to hold image references
 # Elements Initialisation
-exit_btn = ttk.Button(root, text="Quit", command=root.destroy)
+# Labels
 file_explorer = ttk.Label(root, text="Explore files (For the best experience, use files with maximal resolution 600x600)")
+label1 = ttk.Label(root, text="How do you want to watermark your photo?")
+# Buttons
+exit_btn = ttk.Button(root, text="Quit", command=root.destroy)
 browse_btn = ttk.Button(root, text="Browse Folder", command=lambda: browse_files(file_explorer, canvas, img_container))
+# Radio Buttons
+radio_btn1 = ttk.Radiobutton(root, text="Using text", value='txt')
+radio_btn2 = ttk.Radiobutton(root, text="Using image", value='img')
+
 
 # Setting up elements on grid
 file_explorer.pack()
 exit_btn.pack()
 browse_btn.pack()
+label1.pack()
+radio_btn2.pack()
+radio_btn1.pack()
 
 # Canvas setup
 canvas = tk.Canvas(root, height=600, width=600)
