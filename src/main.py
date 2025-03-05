5c6741a38171173219533f11eb6da2841b46fcbe
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-03-05 13:45:42 +0100

    Add command to the button, that matches the add_watermark function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
-from file_actions import browse_files
+from file_actions import browse_files, add_watermark
 
 # Window initialisation and setup
 root = tk.Tk()
@@ -21,6 +21,7 @@ def choose_option():
     if radio_btn_val == 'txt':
         watermark_txt.configure(state='normal')
         watermark_txt.update()
+        watermark_container.append(0)
 
         browse_watermarks_btn.configure(state='disabled')
         browse_watermarks_btn.update()
@@ -46,7 +47,7 @@ label1 = ttk.Label(root, text="How do you want to watermark your photo?")
 exit_btn = ttk.Button(root, text="Quit", command=root.destroy)
 browse_files_btn = ttk.Button(root, text="Browse Files", command=lambda: browse_files(img_container, canvas=canvas, label=file_explorer))
 browse_watermarks_btn = ttk.Button(root, text="Browse watermarks", command=lambda: browse_files(watermark_container), state='disabled')
-add_watermark_btn = ttk.Button(root, text="Add your watermark")
+add_watermark_btn = ttk.Button(root, text="Add your watermark", command=lambda : add_watermark(option_var, img_container[0], watermark_container[0], input_text))
 # Radio Buttons
 radio_btn1 = ttk.Radiobutton(root, text="Using text", variable=option_var, value='txt', command=choose_option)
 radio_btn2 = ttk.Radiobutton(root, text="Using image", variable=option_var, value='img', command=choose_option)
