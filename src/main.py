56184e143cee63d712491b9d9227e23c75ff8498
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-03-05 13:04:00 +0100

    Change status of button to disabled by default

--- src/main.py
+++ src/main.py
@@ -45,7 +45,7 @@ label1 = ttk.Label(root, text="How do you want to watermark your photo?")
 # Buttons
 exit_btn = ttk.Button(root, text="Quit", command=root.destroy)
 browse_files_btn = ttk.Button(root, text="Browse Files", command=lambda: browse_files(img_container, canvas=canvas, label=file_explorer))
-browse_watermarks_btn = ttk.Button(root, text="Browse watermarks", command=lambda: browse_files(watermark_container))
+browse_watermarks_btn = ttk.Button(root, text="Browse watermarks", command=lambda: browse_files(watermark_container), state='disabled')
 add_watermark_btn = ttk.Button(root, text="Add your watermark")
 # Radio Buttons
 radio_btn1 = ttk.Radiobutton(root, text="Using text", variable=option_var, value='txt', command=choose_option)
