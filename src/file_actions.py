63f85e5d26551dc36f8fb4b38b6959df7fdfd043
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-03-07 21:27:38 +0100

    Write function to save image

--- src/file_actions.py
+++ src/file_actions.py
@@ -1,4 +1,4 @@
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 from PIL import Image, ImageTk, ImageDraw, ImageFont
 
 
@@ -28,6 +28,7 @@ def display_image(photo, canvas, img_container, paths_cont):
 # TODO: Think about required arguments, and about setting kwargs
 # TODO: Replace objects with paths to work w/ them within the func
 def add_watermark(method, img_path, watermark, watermark_txt):
+    watermarked = Image.open(img_path)
     font = ImageFont.load_default(40)
     font_col = (128, 128, 128)
     if method == 'txt':
@@ -38,12 +39,18 @@ def add_watermark(method, img_path, watermark, watermark_txt):
         text_w = font.getmask(watermark_txt).getbox()[2]
         text_h = font.getmask(watermark_txt).getbox()[3] + descent
         # Calculate text position
-        x = (watermarked.width() - text_w) / 2
-        y = (watermarked.height() - text_h) / 2
+        x = (watermarked.width - text_w) / 2
+        y = (watermarked.height - text_h) / 2
 
         draw.text((x, y), watermark_txt, font=font, fill=font_col)
-        watermarked.save()
+
         # TODO: Write functionality to add text watermark
     elif method == 'img':
         pass
         # TODO: Write functionality to add image watermark
+
+
+def save_image(watermarked_img):
+    save_path = asksaveasfilename(defaultextension='.jpg', filetypes=[("JPEG Image", '*.jpg'), ("PNG Image", '*.png')])
+    if save_path:
+        watermarked_img.save(save_path)
